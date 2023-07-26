0a51f334cbef3336ecfaffd83fbf929842929eab
Vladyslava Klovak <vladaklovak@gmail.com>
2023-07-26 15:27:54 +0200

    Create header element, add motion

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className}   bg-gray-100  text-gray-950 relative`}
+        className={`${inter.className}   bg-gray-100  text-gray-950 relative  h-[3000px]`}
       >
-        <div className="bg-[#fef9c3]  absolute top-[-6rem] -z-10 right[-8rem] h-[20rem] w-[20rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div className="bg-[#fef9c3]  absolute top-[-6rem] -z-10 right[-8rem] h-[25rem] w-[10rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <Header />
         {children}
       </body>
     </html>
