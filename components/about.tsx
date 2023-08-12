61990f2b5f18d1143d3846e59cafbbb2b1286a74
Vladyslava Klovak <vladaklovak@gmail.com>
2023-08-12 20:15:39 +0200

    Add hooks

--- components/about.tsx
+++ components/about.tsx
@@ -3,8 +3,11 @@
 import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function About() {
+  const { ref } = useSectionInView("About");
+
   return (
     <motion.section
       className="mb-28 max-w-[45rem] leading-8 sm:mb-40 scroll-mt-28"
